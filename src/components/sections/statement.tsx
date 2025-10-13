71450e44521200aa4220ee4775697b7d37e26f69
Inioluwa Oladipupo <officialinioluwaoladipupo@gmail.com>
2025-10-13 11:29:17 +0000

    Increase text size

--- src/components/sections/statement.tsx
+++ src/components/sections/statement.tsx
@@ -8,7 +8,7 @@ export default function Statement() {
     >
       <div className="container mx-auto px-4">
         <div className="text-center max-w-3xl mx-auto">
-          <h2 className="mb-6 text-3xl md:text-4xl font-bold text-primary font-headline !leading-tight">
+          <h2 className="mb-6 text-4xl md:text-5xl font-bold text-primary font-headline !leading-tight">
             <ScrollFloat>
               Architecture has no borders
             </ScrollFloat>
