e7037dc6eef6c5c489d61aa17a7814c88706649e
Inioluwa Oladipupo <officialinioluwaoladipupo@gmail.com>
2025-10-13 19:32:50 +0000

    Pause code page for now

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/codes',
+        destination: '/',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
